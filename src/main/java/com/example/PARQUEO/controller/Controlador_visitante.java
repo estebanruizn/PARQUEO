23fdba65024e9d9bc29ba0493204b7224942a242
Esteban Ruiz Novoa <esteban.ruiz@uniminuto.edu.co>
2023-05-17 22:07:59 -0500

    CREACION DE CONTROLADOR VISITANTE

--- src/main/java/com/example/PARQUEO/controller/Controlador_visitante.java
+++ src/main/java/com/example/PARQUEO/controller/Controlador_visitante.java
@@ -1,4 +1,14 @@
 package com.example.PARQUEO.controller;
 
+import com.example.PARQUEO.impservice.IVisitanteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
 public class Controlador_visitante {
+    @Autowired
+    private IVisitanteService servvisit;
+
 }
