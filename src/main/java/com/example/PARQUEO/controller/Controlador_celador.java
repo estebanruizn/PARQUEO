d42754880e630b0251266d7cee9fa3069998bca7
Esteban Ruiz Novoa <esteban.ruiz@uniminuto.edu.co>
2023-05-23 20:43:23 -0500

    CREACION DE CONTROLADOR LUGAR Y CELADOR

--- src/main/java/com/example/PARQUEO/controller/Controlador_celador.java
+++ src/main/java/com/example/PARQUEO/controller/Controlador_celador.java
@@ -1,4 +1,65 @@
 package com.example.PARQUEO.controller;
 
+import com.example.PARQUEO.Model.Celador;
+import com.example.PARQUEO.impservice.ICeladorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
 public class Controlador_celador {
+
+    @Autowired
+    private ICeladorService servcelador;
+
+    @GetMapping("/celador")
+    public String listar(Model model){
+        model.addAttribute("titulo_celador","CELADORES");
+        model.addAttribute("cuerpo_celador","Proveedores");
+
+        List<Celador> cela = servcelador.listar_celador();
+
+
+        model.addAttribute("datos_celador",cela);
+        return "celador";
+    }
+
+    @GetMapping("/nuevo_celador")
+    public String AgregarCelador(Model model){
+
+        model.addAttribute("titulo_nuevo_celador","Nuevo guarda");
+        model.addAttribute("cuerpo_nuevo_celador","GUARDA NUEVO");
+        model.addAttribute("celador",new Celador());
+
+        return "nuevo_celador";
+    }
+
+    @PostMapping("/guardar_celador")
+    public String Guardarcelador(@ModelAttribute Celador cela){
+
+        servcelador.guardar_celador(cela);
+
+        return "redirect:/celador";
+    }
+
+    @GetMapping("/editar_celador/{id}")
+    public String editarCelador(@PathVariable("id") int id, Model model){
+
+
+        model.addAttribute("celador",servcelador.editar_celador(id));
+
+        return "nuevo_celador";
+    }
+
+    @GetMapping("/eliminar_celador/{id}")
+    public String eliminarCelador(@PathVariable("id") int id, Model model){
+
+        servcelador.eliminar_celador(id);
+        return "redirect:/celador";
+    }
+
 }
--- src/main/java/com/example/PARQUEO/controller/Controlador_lugar.java
+++ src/main/java/com/example/PARQUEO/controller/Controlador_lugar.java
@@ -1,4 +1,48 @@
 package com.example.PARQUEO.controller;
 
+import com.example.PARQUEO.Model.Lugar;
+import com.example.PARQUEO.impservice.ILugarService;
+import com.example.PARQUEO.interfaces.ILugar;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping
 public class Controlador_lugar {
+    @Autowired
+    private ILugarService servlugar;
+
+    @GetMapping("/lugar")
+    public String listar_lugar(Model model){
+
+        model.addAttribute("titulo_lugar","LUGARES");
+        model.addAttribute("cuerpo_lugar","Lugares");
+
+        List<Lugar> lug = servlugar.listar_lugar();
+
+
+        model.addAttribute("datos_lugar",lug);
+        return "lugares";
+    }
+    @PostMapping("/guardar_lugar")
+    public String Guardarlugar(@ModelAttribute Lugar lug){
+
+        servlugar.guardar_lugar(lug);
+
+        return "redirect:/lugares";
+    }
+
+    @GetMapping("/editar_lugar/{id}")
+    public String editarLugar(@PathVariable("id") int id, Model model){
+
+
+        model.addAttribute("lugar",servlugar.editar_lugar(id));
+
+        return "nuevo_lugar";
+    }
+
 }
